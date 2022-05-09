addb8f00443748229e6e5991ff2b550d51d9563a
Thalys RD <tgbr66@gmail.com>
2022-05-09 17:46:00 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente "Favorite Pokemons"', () => {
+  it(`Testa se, caso a pessoa não tenha pokemons
+   favoritos, aparece a mensagem "No favorite pokemon found"`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const noFavoriteFound = screen.getByText('No favorite pokemon found');
+    expect(noFavoriteFound).toBeInTheDocument();
+  });
+  it('Testa se, são exibidos os cards dos pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const button = screen.getByRole('checkbox', { name: /Favoritado/i });
+    expect(button).toBeInTheDocument();
+    userEvent.click(button);
+    history.push('/pokemons/4');
+    userEvent.click(button);
+    history.push('/favorites');
+    const title = 'Favorite pokémons';
+    const favPokeCheck = screen.getByRole('heading', { name: title, level: 2 });
+    expect(favPokeCheck).toBeInTheDocument();
+    const pikachuCheck = screen.getByText('Pikachu');
+    const charmandeCheck = screen.getByText('Charmander');
+    expect(pikachuCheck).toBeInTheDocument();
+    expect(charmandeCheck).toBeInTheDocument();
+  });
+});
