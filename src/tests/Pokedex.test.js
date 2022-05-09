403ffb3dcfc977793ed7d1452436237bee75c78c
Thalys RD <tgbr66@gmail.com>
2022-05-09 18:10:35 -0300

    Requisito 4 corrigido dnv

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,4 +3,13 @@ import React from 'react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-describe('Testa o componente Pokedex.js')
+describe('Testa o componente Pokedex.js', () => {
+  it('Testa se a página possui o texto "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+    const h2El = screen.getByRole('heading', { name: 'Encountered pokémons', level: 2 });
+    expect(h2El).toBeInTheDocument();
+  });
+  it('Testa se é exibido o próximo pokemon da lista, quando o botão for clicado', () => {
+
+  });
+});
