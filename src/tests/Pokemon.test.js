3fa3a5cd434dca87b8fff42b2a038f4dc7f98a28
Thalys RD <tgbr66@gmail.com>
2022-05-10 17:37:31 -0300

    Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente "Pokemon.js"', () => {
+  it('Testa se é renderizado um card com as informações de determinado Pokémon', () => {
+    renderWithRouter(<App />);
+    const pokeName = screen.getByTestId('pokemon-name');
+    const pokeType = screen.getByTestId('pokemon-type');
+    const pokeWeight = screen.getByTestId('pokemon-weight');
+    const pokeImg = screen.getByRole('img', { name: /sprite/i });
+    const pokeLink = screen.getByRole('link', { name: /More details/i });
+    const pokeType2 = screen.queryAllByText('Electric');
+
+    expect(pokeName).toBeInTheDocument();
+    expect(pokeType2[0]).toBeDefined();
+    expect(pokeType).toHaveTextContent('Electric');
+    expect(pokeWeight).toBeInTheDocument();
+    expect(pokeImg).toBeInTheDocument();
+    expect(pokeImg.src).toContain('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(pokeLink).toBeInTheDocument();
+    expect(pokeLink).toHaveAttribute('href', '/pokemons/25');
+    userEvent.click(pokeLink);
+
+    const favButton = screen.getByRole('checkbox', { name: /favoritado/i });
+    userEvent.click(favButton);
+    const startEl = screen.getByRole('img', { name: /as favorite/i });
+    expect(startEl).toBeInTheDocument();
+    expect(startEl.src).toContain('http://localhost/star-icon.svg');
+  });
+});
